e88aa3411f274aeaae2fead0a9a7e454b4778417
Hiroki Osame <hiroki.osame@gmail.com>
2025-10-12 10:34:53 +0900

    chore(deps): remove axios and vue test dependencies
    Removed real-packages test suite that tested against axios and vue packages. These dependencies created maintenance burden requiring constant security updates for packages not actually used in production.

--- tests/index.ts
+++ tests/index.ts
@@ -4,7 +4,6 @@ describe('pkg-entry-points', ({ runTestSuite }) => {
 	runTestSuite(import('./specs/pkg-entry-points.js'));
 	runTestSuite(import('./specs/exports-types.js'));
 	runTestSuite(import('./specs/null-block.js'));
-	runTestSuite(import('./specs/real-packages.js'));
 	runTestSuite(import('./specs/merge-conditions.js'));
 	runTestSuite(import('./specs/no-exports.js'));
 

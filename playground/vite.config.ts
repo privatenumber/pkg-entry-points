b80bea3f5aac69507411311d83b72bc2025f783c
Hiroki Osame <hiroki.osame@gmail.com>
2025-10-13 14:02:40 +0900

    wip

---
+++ playground/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+	],
+	optimizeDeps: {
+		include: ['monaco-editor'],
+	},
+});

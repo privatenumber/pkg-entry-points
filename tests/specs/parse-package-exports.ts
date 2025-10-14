1dd431c9f53cab3fb46e8cb7deb66e450150e513
Hiroki Osame <hiroki.osame@gmail.com>
2025-10-14 22:34:24 +0900

    test: add empty object test for parsePackageExports

--- tests/specs/parse-package-exports.ts
+++ tests/specs/parse-package-exports.ts
@@ -20,5 +20,11 @@ export default testSuite(({ describe }) => {
 
 			expect(result).toStrictEqual([]);
 		});
+
+		test('empty object', () => {
+			const result = parsePackageExports({});
+
+			expect(result).toStrictEqual([]);
+		});
 	});
 });

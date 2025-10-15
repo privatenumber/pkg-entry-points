784bade746988c3ba78b3af25a7f931ae510911b
Hiroki Osame <hiroki.osame@gmail.com>
2025-10-15 19:16:53 +0900

    wip

--- playground/src/examples.ts
+++ playground/src/examples.ts
@@ -1,23 +1,23 @@
 import type { PackageJsonWithName } from './utils/parse-package-json.js';
 
-export const basic ={
-    name: 'example-package',
-    exports: {
-        '.': './index.js',
-        './utils': './utils.js',
-    },
+export const basic = {
+	name: 'example-package',
+	exports: {
+		'.': './index.js',
+		'./utils': './utils.js',
+	},
 } satisfies PackageJsonWithName;
 
 export const wildcardDanger = {
-    name: 'dangerous-wildcards',
-    exports: {
-        './*': './dist/*.js',
-    },
+	name: 'dangerous-wildcards',
+	exports: {
+		'./*': './dist/*.js',
+	},
 } satisfies PackageJsonWithName;
 
 export const wildcardSafe = {
-    name: 'safe-wildcards',
-    exports: {
-        './features/*': './dist/features/*/index.js',
-    },
+	name: 'safe-wildcards',
+	exports: {
+		'./features/*': './dist/features/*/index.js',
+	},
 } satisfies PackageJsonWithName;
